09a94da1463fbb8081f169585d1cad7e8fd7517a
Yokoyama Hirokazu <hiroyky.voyager@gmail.com>
2020-05-30 02:03:18 +0900

    GraphQLのスキーマ設計 (#3)
    * 初期のgqlgen.yml
    
    * gqlgen.ymlを更新
    
    * GraphQLスキーマ定義
    
    * 生成された物
    
    * mod & sum

---
+++ resolver/schema.resolvers.go
@@ -0,0 +1,20 @@
+package resolver
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hiroyky/nikki_backend/domain/gqlmodel"
+)
+
+func (r *queryResolver) Article(ctx context.Context, id string) (*gqlmodel.Article, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Query returns QueryResolver implementation.
+func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
