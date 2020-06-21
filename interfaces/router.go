186328d2dbf8f9af5b20b716687ce76b4ebbbe50
Yokoyama Hirokazu <hiroyky.voyager@gmail.com>
2020-06-22 01:31:51 +0900

    GraphQLのエンドポイントを改修

--- interfaces/router.go
+++ interfaces/router.go
@@ -1,21 +1,30 @@
 package interfaces
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gin-gonic/gin"
+	"github.com/hiroyky/nikki_backend/resolvers/adminresolvers"
+	"github.com/hiroyky/nikki_backend/resolvers/viewerresolvers"
 )
 
 var Engine *gin.Engine
+var adminServer *handler.Server
+var viewerServer *handler.Server
 
 func init() {
 	r := gin.Default()
+	adminServer = handler.NewDefaultServer(adminresolvers.NewExecutableSchema(adminresolvers.Config{Resolvers: &adminresolvers.Resolver{}}))
+	viewerServer = handler.NewDefaultServer(viewerresolvers.NewExecutableSchema(viewerresolvers.Config{Resolvers: &viewerresolvers.Resolver{}}))
 
 	admin := r.Group("/admin")
 	{
 		admin.GET("/", func(ctx *gin.Context) {
 			playground.Handler("Admin graphql", "/admin/query").ServeHTTP(ctx.Writer, ctx.Request)
 		})
-		admin.POST("/query")
+		admin.POST("/query", func(ctx *gin.Context) {
+			adminServer.ServeHTTP(ctx.Writer, ctx.Request)
+		})
 	}
 
 	viewer := r.Group("/viewer")
@@ -23,8 +32,9 @@ func init() {
 		viewer.GET("/", func(ctx *gin.Context) {
 			playground.Handler("Viewer graphql", "/viewer/query").ServeHTTP(ctx.Writer, ctx.Request)
 		})
-		viewer.POST("/query")
+		viewer.POST("/query", func(ctx *gin.Context) {
+			viewerServer.ServeHTTP(ctx.Writer, ctx.Request)
+		})
 	}
-
 	Engine = r
 }
