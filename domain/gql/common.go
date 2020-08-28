11769a12704903a4a79886332b54afcf3956c017
Yokoyama Hirokazu <hiroyky.voyager@gmail.com>
2020-08-29 05:50:56 +0900

    モデルを定義

--- domain/gql/common.go
+++ domain/gql/common.go
@@ -1,1 +1,12 @@
 package gql
+type SortOrder struct {
+	Sort  string `json:"sort"`
+	Order *Order `json:"order"`
+}
+
+type Order string
+
+const (
+	OrderAsc  Order = "ASC"
+	OrderDesc Order = "DESC"
+)
