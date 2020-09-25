e28ac339c7c5c439ca0ff0ee63426b3b2ee74f37
Yokoyama Hirokazu <hiroyky.voyager@gmail.com>
2020-09-25 21:42:08 +0900

    改修 (#23)

--- registry/viewer_resolver.go
+++ registry/viewer_resolver.go
@@ -8,13 +8,6 @@ var ViewerResolverConfig viewerresolvers.Config
 
 func init() {
 	ViewerResolverConfig = viewerresolvers.Config{}
-	ViewerResolverConfig.Complexity = newViewerComplexRoot()
+	ViewerResolverConfig.Complexity = *viewerresolvers.ViewerComplexRoot
 	ViewerResolverConfig.Resolvers = &viewerresolvers.Resolver{}
-}
-
-func newViewerComplexRoot() viewerresolvers.ComplexityRoot {
-	root := viewerresolvers.ComplexityRoot{}
-	root.Query.Articles = viewerresolvers.ArticlesComplexity
-	root.Query.Article = viewerresolvers.ArticleComplexity
-	return root
 }
