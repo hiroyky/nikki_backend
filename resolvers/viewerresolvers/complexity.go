bafb2b837bb1445bf2b7ce5902a575685b968af9
Yokoyama Hirokazu <hiroyky.voyager@gmail.com>
2020-09-25 21:41:41 +0900

    改修

--- resolvers/viewerresolvers/complexity.go
+++ resolvers/viewerresolvers/complexity.go
@@ -6,11 +6,25 @@ import (
 	"github.com/hiroyky/nikki_backend/service"
 )
 
-func ArticlesComplexity(childComplexity int, sort []*gql.SortOrder, page *viewermodel.Pagination) int {
-	limit, _ := service.ValidateViewerPagination(page, 10, 100)
-	return limit
-}
+var ViewerComplexRoot *ComplexityRoot = nil
 
-func ArticleComplexity(childComplexity int, id string) int {
-	return 1
+func init() {
+	root := &ComplexityRoot{}
+
+	root.Article.PreviousArticle = func(childComplexity int) int {
+		return childComplexity + 5
+	}
+	root.Article.NextArticle = func(childComplexity int) int {
+		return childComplexity + 5
+	}
+
+	root.Query.Articles = func(childComplexity int, sort []*gql.SortOrder, page *viewermodel.Pagination) int {
+		limit, _ := service.ValidateViewerPagination(page, 10, 100)
+		return limit * childComplexity
+	}
+	root.Query.Article = func(childComplexity int, id string) int {
+		return childComplexity + 1
+	}
+
+	ViewerComplexRoot = root
 }
